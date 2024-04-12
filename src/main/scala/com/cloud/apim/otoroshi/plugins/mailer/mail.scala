1a107ba72ff96a57c5d5d67aab9525f6a584b0ec
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2024-04-12 12:18:40 +0200

    handle attachments

--- src/main/scala/com/cloud/apim/otoroshi/plugins/mailer/mail.scala
+++ src/main/scala/com/cloud/apim/otoroshi/plugins/mailer/mail.scala
@@ -3,18 +3,24 @@ package otoroshi_plugins.com.cloud.apim.otoroshi.plugins.mailer
 import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
 import akka.stream.{Materializer, OverflowStrategy, QueueOfferResult}
 import akka.util.ByteString
+import com.google.common.base.Charsets
 import com.sun.mail.smtp.SMTPTransport
+import org.joda.time.DateTime
 import otoroshi.env.Env
+import otoroshi.events.AlertEvent
+import otoroshi.models.ApiKey
 import otoroshi.next.plugins.api._
 import otoroshi.next.proxy.NgProxyEngineError
 import otoroshi.utils.syntax.implicits._
 import play.api.libs.json._
 import play.api.mvc.Results
 
-import java.util.Properties
+import java.util.{Properties, UUID}
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicReference
-import javax.mail.internet.{InternetAddress, MimeMessage}
+import javax.activation.DataHandler
+import javax.mail.internet.{InternetAddress, MimeBodyPart, MimeMessage, MimeMultipart}
+import javax.mail.util.ByteArrayDataSource
 import javax.mail.{Message, Session}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util._
@@ -26,7 +32,8 @@ case class MailerApiConfiguration(
   password: Option[String] = None,
   auth: Boolean = false,
   starttlsEnabled: Boolean = false,
-  smtps: Boolean = true
+  smtps: Boolean = true,
+  maxRetries: Int = 5,
 ) extends NgPluginConfig {
   def json: JsValue = MailerApiConfiguration.format.writes(this)
 }
@@ -42,6 +49,7 @@ object MailerApiConfiguration {
       "auth" -> o.auth,
       "starttls_enabled" -> o.starttlsEnabled,
       "smtps" -> o.smtps,
+      "max_retries" -> o.maxRetries,
     )
     override def reads(json: JsValue): JsResult[MailerApiConfiguration] = Try {
       MailerApiConfiguration(
@@ -52,6 +60,7 @@ object MailerApiConfiguration {
         auth = json.select("auth").asOpt[Boolean].getOrElse(false),
         starttlsEnabled = json.select("starttls_enabled").asOpt[Boolean].getOrElse(false),
         smtps = json.select("smtps").asOpt[Boolean].getOrElse(true),
+        maxRetries = json.select("max_retries").asOpt[Int].getOrElse(5),
       )
     } match {
       case Failure(e) => JsError(e.getMessage)
@@ -66,6 +75,7 @@ object MailerApiConfiguration {
     "auth",
     "starttls_enabled",
     "smtps",
+    "max_retries",
   )
   val configSchema = Some(Json.obj(
     "host" -> Json.obj("type" -> "string", "label" -> "Host"),
@@ -75,15 +85,48 @@ object MailerApiConfiguration {
     "auth" -> Json.obj("type" -> "bool", "label" -> "Authentication"),
     "starttls_enabled" -> Json.obj("type" -> "bool", "label" -> "StartTLS"),
     "smtps" -> Json.obj("type" -> "bool", "label" -> "TLS"),
+    "max_retries" -> Json.obj("type" -> "number", "label" -> "Max retries"),
   ))
 }
 
-case class Mail(subject: String, from: String, to: Seq[String], cc: Seq[String], bcc: Seq[String], content: ByteString, mimetype: String, config: MailerApiConfiguration)
+case class MailAttachment(name: String, content: ByteString, mimetype: String, disposition: String)
+
+// TODO: support attachments
+case class Mail(
+   subject: String,
+   from: String,
+   to: Seq[String],
+   cc: Seq[String],
+   bcc: Seq[String],
+   content: ByteString,
+   mimetype: String,
+   config: MailerApiConfiguration,
+   tryCount: Int,
+   attachments: Seq[MailAttachment]
+) {
+  def json: JsValue = Json.obj(
+    "subject" -> subject,
+    "from" -> from,
+    "to" -> to,
+    "cc" -> cc,
+    "bcc" -> bcc,
+    "content" -> content.utf8String,
+    "mimetype" -> mimetype,
+    "config" -> (config.json.asObject - "password" ++ Json.obj("password" -> "****")),
+    "try_count" -> tryCount,
+    "attachments" -> JsArray(attachments.map(a => Json.obj(
+      "name" -> a.name,
+      "content" -> a.content.encodeBase64.utf8String,
+      "mimetype" -> a.mimetype,
+      "disposition" -> a.disposition
+    ))),
+  )
+}
 
 class MailerApi extends NgBackendCall {
 
   override def steps: Seq[NgStep] = Seq(NgStep.CallBackend)
-  override def categories: Seq[NgPluginCategory] = Seq(NgPluginCategory.Custom("Mailing"))
+  override def categories: Seq[NgPluginCategory] = Seq(NgPluginCategory.Custom("Mailing"), NgPluginCategory.Custom("Cloud APIM"))
   override def visibility: NgPluginVisibility = NgPluginVisibility.NgUserLand
   override def multiInstance: Boolean = true
   override def core: Boolean = true
@@ -130,18 +173,49 @@ class MailerApi extends NgBackendCall {
     mail.cc.foreach(to => msg.addRecipient(Message.RecipientType.CC, new InternetAddress(to)))
     mail.bcc.foreach(to => msg.addRecipient(Message.RecipientType.BCC, new InternetAddress(to)))
     msg.setSubject(mail.subject)
-    msg.setContent(mail.content.utf8String, mail.mimetype)
+    if (mail.attachments.isEmpty) {
+      msg.setContent(mail.content.utf8String, mail.mimetype)
+    } else {
+      val messageBodyPart = new MimeBodyPart()
+      messageBodyPart.setContent(mail.content.utf8String, mail.mimetype)
+      val attachements = mail.attachments.map { att =>
+        val attachmentPart = new MimeBodyPart()
+        attachmentPart.setFileName(att.name)
+        if (att.disposition == "inline") {
+          attachmentPart.setContentID(s"<${att.name}>")
+          attachmentPart.setDisposition(javax.mail.Part.INLINE)
+        }
+        attachmentPart.setDataHandler(new DataHandler(new ByteArrayDataSource(att.content.toArray, att.mimetype)))
+        attachmentPart
+      }
+      val multipart = new MimeMultipart("related")
+      multipart.addBodyPart(messageBodyPart)
+      attachements.foreach(att => multipart.addBodyPart(att))
+      msg.setContent(multipart)
+    }
+    val addresses = mail.to ++ mail.cc ++ mail.bcc
 
     Future {
       Using(session.getTransport(protocol).asInstanceOf[SMTPTransport]) { transport =>
-        {
-          transport.connect(config.host, config.user.getOrElse(mail.from), config.password.orNull)
-          transport.sendMessage(msg, msg.getAllRecipients)
+        transport.connect(config.host, config.user.getOrElse(mail.from), config.password.orNull)
+        transport.sendMessage(msg, msg.getAllRecipients)
+      }
+      .toEither
+      .left.map { t =>
+        if (mail.tryCount < config.maxRetries) {
+          env.logger.error(s"error while trying to send email to ${addresses.mkString(", ")}, retrying ...", t)
+          getQueueRef().offer(mail.copy(tryCount = mail.tryCount + 1))
+        } else {
+          env.logger.error(s"error while trying to send email to ${addresses.mkString(", ")}, publishing alert ...", t)
+          EmailSendingError(UUID.randomUUID().toString, mail).toAnalytics()
         }
-      }.toEither.left.map(t => {
-        t.printStackTrace()
         t
-      })
+      }
+      .right.map { r =>
+        env.logger.info(s"email sent to ${addresses.mkString(", ")}")
+        EmailSendingSuccess(UUID.randomUUID().toString, mail).toAnalytics()
+        r
+      }
     }(sendingEc)
   }
 
@@ -152,9 +226,24 @@ class MailerApi extends NgBackendCall {
       to = value.select("to").asOpt[Seq[String]].getOrElse(Seq.empty),
       cc = value.select("cc").asOpt[Seq[String]].getOrElse(Seq.empty),
       bcc = value.select("bcc").asOpt[Seq[String]].getOrElse(Seq.empty),
-      mimetype = value.select("mimetype").asOpt[String].getOrElse("text/html; charset=utf-8"),
+      mimetype = value.select("mimetype").asOpt[String] match {
+        case Some(mimetype) => mimetype
+        case None if value.select("html").isDefined => "text/html; charset=utf-8"
+        case None if value.select("text").isDefined => "text/plain; charset=utf-8"
+        case _ => "text/html; charset=utf-8"
+      },
       content = value.select("body").asOpt[String].orElse(value.select("html").asOpt[String]).orElse(value.select("text").asOpt[String]).getOrElse("").byteString,
       config = config,
+      tryCount = 0,
+      attachments = value.select("attachements").asOpt[Seq[JsObject]] match {
+        case None => Seq.empty
+        case Some(seq) => seq.map(obj => MailAttachment(
+          obj.select("name").asString,
+          obj.select("content").asString.byteString.decodeBase64,
+          obj.select("mimetype").asOpt[String].getOrElse("application/octet-stream"),
+          obj.select("disposition").asOpt[String].getOrElse("attachment"),
+        ))
+      }
     )).flatMap {
       case QueueOfferResult.QueueClosed => BackendCallResponse(NgPluginHttpResponse.fromResult(Results.Ok(Json.obj("enqueue" -> false, "error" -> "queue already closed"))), None).rightf
       case QueueOfferResult.Dropped => BackendCallResponse(NgPluginHttpResponse.fromResult(Results.Ok(Json.obj("enqueue" -> false, "error" -> "email dropped"))), None).rightf
@@ -175,4 +264,57 @@ class MailerApi extends NgBackendCall {
       BackendCallResponse(NgPluginHttpResponse.fromResult(Results.NotFound(Json.obj("error" -> "not found"))), None).rightf
     }
   }
+}
+
+case class EmailSendingError(
+   `@id`: String,
+   mail: Mail,
+   `@timestamp`: DateTime = DateTime.now()
+) extends AlertEvent {
+
+  override def `@service`: String   = "Otoroshi"
+  override def `@serviceId`: String = "--"
+
+  override def fromOrigin: Option[String]    = None
+  override def fromUserAgent: Option[String] = None
+
+  override def toJson(implicit _env: Env): JsValue =
+    Json.obj(
+      "@id"           -> `@id`,
+      "@timestamp"    -> play.api.libs.json.JodaWrites.JodaDateTimeNumberWrites.writes(`@timestamp`),
+      "@type"         -> `@type`,
+      "@product"      -> _env.eventsName,
+      "@serviceId"    -> `@serviceId`,
+      "@service"      -> `@service`,
+      "@env"          -> "prod",
+      "alert"         -> "EmailSendingError",
+      "mail" -> mail.json,
+    )
+}
+
+
+case class EmailSendingSuccess(
+                              `@id`: String,
+                              mail: Mail,
+                              `@timestamp`: DateTime = DateTime.now()
+                            ) extends AlertEvent {
+
+  override def `@service`: String   = "Otoroshi"
+  override def `@serviceId`: String = "--"
+
+  override def fromOrigin: Option[String]    = None
+  override def fromUserAgent: Option[String] = None
+
+  override def toJson(implicit _env: Env): JsValue =
+    Json.obj(
+      "@id"           -> `@id`,
+      "@timestamp"    -> play.api.libs.json.JodaWrites.JodaDateTimeNumberWrites.writes(`@timestamp`),
+      "@type"         -> `@type`,
+      "@product"      -> _env.eventsName,
+      "@serviceId"    -> `@serviceId`,
+      "@service"      -> `@service`,
+      "@env"          -> "prod",
+      "alert"         -> "EmailSendingSuccess",
+      "mail" -> mail.json,
+    )
 }
